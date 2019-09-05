cf3912bfdf140c25b06374b6cb9786eeba97640f
Oleksandr Khomenko <homes89@ukr.net>
2019-09-05 22:43:35 +0300

    Setup list component

---
+++ src/components/List/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ *
+ * List
+ *
+ */
+const List = props => {
+  return <div>List</div>;
+};
+
+List.propTypes = {};
+
+export default List;
